0182e5bfcd7791b77f5c10d201e49c7a95b1b5a6
ROSSAT Nicolas <75442011+NicolasROSSAT69@users.noreply.github.com>
2023-08-28 20:08:50 +0200

    First commit

---
+++ lib/first_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("first_screen");
+  }
+}
